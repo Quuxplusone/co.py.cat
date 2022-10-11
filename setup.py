8ec9209a118e5c8a4a5c23a5a999c76163a283e8
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2022-10-11 11:50:04 -0400

    Bump the revision number from 0.0.1 to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='copycat',
-    version='0.0.1',
+    version='0.1.0',
     packages=['copycat'],
     install_requires=[
         # pip requirements go here; at the moment there are none
